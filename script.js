6d26ce7a40078487a0bd3d5d622b01ee4314f20d
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-28 11:25:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var website = Math.floor(Math.random() * 3) + 1;
+
+if (website === 1) {
+  window.location.href = "https://www.google.com/search?sca_esv=792c36b2414f597c&sca_upv=1&rlz=1CAHVCY_enUS1073&q=raven+team+leader&uds=ADvngMjwX4dvqjExbCg9zqvMuh5VKSR5idHzhiFzlkPbr4z5hsjJ2hMF0VY2sroe0AUAu8fnTa1iG2E4zRLd4cduUl-3R4IRmKMwVYnzHqIb5v1DuVif03s7xopVvFh-ixNdISUSW19eAKvW9Sd9OEeniZm2nNmH7HQEXqmwivS0WK43PXutDiwbrNCp7INkIZQ97KpddYOuKhSo5ifUTs1baNH64-bM89W6patwfWHrCGDkpO6PZMW6g9lY1fcOKtLpAeJuntS2AQxixMCzSWEl_rCQyKPp5NXpRNWwZCQoG9GQ4HgkeHcefdCKvFTMHdHdypt9hAZu&udm=2&prmd=ivsnmbtz&sa=X&ved=2ahUKEwjz4oyV07CGAxWYkYkEHRL1CaQQtKgLegQIERAB&safe=active&ssui=on";
+}
