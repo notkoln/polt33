ded17f141ff41031ecfe73773a253c13c8bdf3c5
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-28 11:36:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var website = Math.floor(Math.random() * 3) + 1;
 
-if (website === 1) {
-  window.location.href = "https://www.google.com/search?sca_esv=792c36b2414f597c&sca_upv=1&rlz=1CAHVCY_enUS1073&q=raven+team+leader&uds=ADvngMjwX4dvqjExbCg9zqvMuh5VKSR5idHzhiFzlkPbr4z5hsjJ2hMF0VY2sroe0AUAu8fnTa1iG2E4zRLd4cduUl-3R4IRmKMwVYnzHqIb5v1DuVif03s7xopVvFh-ixNdISUSW19eAKvW9Sd9OEeniZm2nNmH7HQEXqmwivS0WK43PXutDiwbrNCp7INkIZQ97KpddYOuKhSo5ifUTs1baNH64-bM89W6patwfWHrCGDkpO6PZMW6g9lY1fcOKtLpAeJuntS2AQxixMCzSWEl_rCQyKPp5NXpRNWwZCQoG9GQ4HgkeHcefdCKvFTMHdHdypt9hAZu&udm=2&prmd=ivsnmbtz&sa=X&ved=2ahUKEwjz4oyV07CGAxWYkYkEHRL1CaQQtKgLegQIERAB&safe=active&ssui=on";
-}
+setTimeout(() => {
+  if (website === 1) {
+    window.location.href = "https://www.google.com/search?sca_esv=792c36b2414f597c&sca_upv=1&rlz=1CAHVCY_enUS1073&q=raven+team+leader&uds=ADvngMjwX4dvqjExbCg9zqvMuh5VKSR5idHzhiFzlkPbr4z5hsjJ2hMF0VY2sroe0AUAu8fnTa1iG2E4zRLd4cduUl-3R4IRmKMwVYnzHqIb5v1DuVif03s7xopVvFh-ixNdISUSW19eAKvW9Sd9OEeniZm2nNmH7HQEXqmwivS0WK43PXutDiwbrNCp7INkIZQ97KpddYOuKhSo5ifUTs1baNH64-bM89W6patwfWHrCGDkpO6PZMW6g9lY1fcOKtLpAeJuntS2AQxixMCzSWEl_rCQyKPp5NXpRNWwZCQoG9GQ4HgkeHcefdCKvFTMHdHdypt9hAZu&udm=2&prmd=ivsnmbtz&sa=X&ved=2ahUKEwjz4oyV07CGAxWYkYkEHRL1CaQQtKgLegQIERAB&safe=active&ssui=on";
+  }
+  if (website === 2) {
+    window.location.href = "https://www.google.com/search?sca_esv=792c36b2414f597c&sca_upv=1&rlz=1CAHVCY_enUS1073&q=vaporeon+deviant+art&uds=ADvngMjwX4dvqjExbCg9zqvMuh5VKSR5idHzhiFzlkPbr4z5hsjJ2hMF0VY2sroe0AUAu8fnTa1iG2E4zRLd4cduUl-3R4IRmKMwVYnzHqIb5v1DuVif03s7xopVvFh-ixNdISUSW19eAKvW9Sd9OEeniZm2nNmH7HQEXqmwivS0WK43PXutDiwbrNCp7INkIZQ97KpddYOuKhSo5ifUTs1baNH64-bM89W6patwfWHrCGDkpO6PZMW6g9lY1fcOKtLpAeJuntS2AQxixMCzSWEl_rCQyKPp5NXpRNWwZCQoG9GQ4HgkeHcefdCKvFTMHdHdypt9hAZu&udm=2&prmd=ivsnmbtz&sa=X&ved=2ahUKEwjz4oyV07CGAxWYkYkEHRL1CaQQtKgLegQIERAB&safe=active&ssui=on";
+  }
+  if (website === 3) {
+    window.location.href = "https://www.google.com/search?sca_esv=792c36b2414f597c&sca_upv=1&rlz=1CAHVCY_enUS1073&q=god+i+love+trampolining&uds=ADvngMjwX4dvqjExbCg9zqvMuh5VKSR5idHzhiFzlkPbr4z5hsjJ2hMF0VY2sroe0AUAu8fnTa1iG2E4zRLd4cduUl-3R4IRmKMwVYnzHqIb5v1DuVif03s7xopVvFh-ixNdISUSW19eAKvW9Sd9OEeniZm2nNmH7HQEXqmwivS0WK43PXutDiwbrNCp7INkIZQ97KpddYOuKhSo5ifUTs1baNH64-bM89W6patwfWHrCGDkpO6PZMW6g9lY1fcOKtLpAeJuntS2AQxixMCzSWEl_rCQyKPp5NXpRNWwZCQoG9GQ4HgkeHcefdCKvFTMHdHdypt9hAZu&udm=2&prmd=ivsnmbtz&sa=X&ved=2ahUKEwjz4oyV07CGAxWYkYkEHRL1CaQQtKgLegQIERAB&safe=active&ssui=on";
+  }
+), 1500}
